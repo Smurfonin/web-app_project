4af5fb92985e7ea3c84f7ac7a4ab647992f303d8
Smurfonin <smurfoninan@gmail.com>
2024-12-07 17:20:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import unittest
+import random
 
+# Функция для деления двух чисел
+def divide(a, b):
+    if b == 0:
+        raise ValeuError("Деление на ноль недопустимо.")
+      return a / b
+
+# Функция для проверки четности числа
+def is_even(num):
+    return num % 2 == 0
+
+class TestMathOperations(unittest.TestCase):
+
+    def test_divide(self):
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+      print(f"Тест: делим {a} на {b}")
+      result = divide(a, b)
+      print(f"Результат: {result}")
+      self.assertIsIntance(result, float)
+
+      def test_is_even(self):
+          num = random.randint(1, 100)
+          print(f"Тест: число {num} является {'четным' if is_even(num) else 'нечетным'}.")
+
+if __name__ == "__main__":
+    unittest.main()
